be549eaff65e1f2ee2cde94c01d640acc65c9b47
yetsyl1997 <67623921+yeseul97@users.noreply.github.com>
2022-06-23 20:29:49 +0900

    Time: 84 ms (32.16%), Space: 19.3 MB (6.33%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,13 +1,15 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        strs = []
+        # 자료형 데크로 선언 - 데크=양방향 큐
+        strs: Deque = collections.deque()
+            
         for char in s:
-            if char.isalnum(): # 문자열이 영어,한글,숫자로 되어있으면 참 리턴,아니면 거짓 리턴
-                strs.append(char.lower()) # True이면 소문자로 바꿔서 추가
+            if char.isalnum():
+                strs.append(char.lower())
                 
-        # 팰린드롬 여부 판별
         while len(strs) > 1:
-            if strs.pop(0) != strs.pop():
+            if strs.popleft() != strs.pop():
                 return False
             
         return True
+                
